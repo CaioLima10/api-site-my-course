5c17099fddb72b5764b90627bdeda7ad51c0be3f
Caio lima <caiohenrys50@gmail.com>
2024-03-03 23:55:13 -0300

    feat: add cors and parser json

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import userRouter from "./routes/user.js";
+import authRouter from "./routes/auth.js";
+import postRouter from "./routes/post.js";
+import commentRouter from "./routes/comment.js";
+import likesRouter from "./routes/likes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Allow-Credentials",
+  ],
+};
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/post", postRouter);
+app.use("/api/comment", commentRouter);
+app.use("/api/likes", likesRouter);
+
+app.listen(8005, () => {
+  console.log("Servidor conectado na porta 8001");
+});
